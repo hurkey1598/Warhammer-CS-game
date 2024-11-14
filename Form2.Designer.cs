6f64c492e11f6b6e26c338a46a71ccd19c27fd27
hurkey1598 <hurkey1598@tutanota.com>
2024-11-14 20:37:20 +0000

    Changed background colour of form

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -32,9 +32,11 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 501);
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(672, 617);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form2";
